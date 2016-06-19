0e1272f6305dddf3483fb6093c9f0f15a5e3b1ae
JinJay <jinjaysnow@gmail.com>
2016-06-19 20:14:52 +0800

    add qrcode image

---
+++ static/drop.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $(".shop1 .selLabel").click(
+        function() {
+        $(".shop1.dropdown").toggleClass('active');
+    });
+    $(".shop2 .selLabel").click(
+        function() {
+        $(".shop2.dropdown").toggleClass('active');
+    });
+    $(".shop3 .selLabel").click(
+        function() {
+        $(".shop3.dropdown").toggleClass('active');
+    });
+});
