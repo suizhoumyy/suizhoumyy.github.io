23f94623ce70980476bbc9f050eddae986f0f150
JinJay <jinjaysnow@gmail.com>
2016-06-28 21:46:21 +0800

    modify 神农

---
+++ all_staff/get_all_staff.sh
@@ -0,0 +1,3 @@
+curl http://myy-a2015e8014661092.c9users.io/all_staff/1/ -o ./1/index.html
+curl http://myy-a2015e8014661092.c9users.io/all_staff/2/ -o ./2/index.html
+curl http://myy-a2015e8014661092.c9users.io/all_staff/3/ -o ./3/index.html
