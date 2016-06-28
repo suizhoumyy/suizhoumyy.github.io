40862c22b2ff66e56343e239203fbaac845f8785
JinJay <jinjaysnow@gmail.com>
2016-06-28 21:50:59 +0800

    update info

--- all_staff/get_all_staff.sh
+++ all_staff/get_all_staff.sh
@@ -1,6 +1,6 @@
-curl http://myy-a2015e8014661092.c9users.io/all_staff/1/ -o ./1/index.html
-curl http://myy-a2015e8014661092.c9users.io/all_staff/2/ -o ./2/index.html
-curl http://myy-a2015e8014661092.c9users.io/all_staff/3/ -o ./3/index.html
+curl http://myy-a2015e8014661092.c9users.io/all_staff/1/ -o ./1/index.html.1
+curl http://myy-a2015e8014661092.c9users.io/all_staff/2/ -o ./2/index.html.1
+curl http://myy-a2015e8014661092.c9users.io/all_staff/3/ -o ./3/index.html.1
 mv ./1/index.html.1 ./1/index.html
 mv ./2/index.html.1 ./2/index.html
 mv ./3/index.html.1 ./3/index.html
